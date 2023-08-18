11c38cf1d708ded323cc817bbbd179376ae793ed
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2023-08-18 11:22:04 +0300

    added method to get current login user ID

--- server/src/main/java/org/moviematchers/moviematch/service/UserService.java
+++ server/src/main/java/org/moviematchers/moviematch/service/UserService.java
@@ -81,4 +81,9 @@ public class UserService {
             );
         }
     }
+
+    public Long getLoginUserID(String username) {
+        MovieUser user = userRepository.findByUserName(username);
+        return user.getUserID();
+    }
 }
