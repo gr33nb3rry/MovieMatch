79741591fc9312c5dbbb66066517e9ec23cc600f
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2023-08-19 01:51:39 +0300

    removed useless entity fields

--- server/src/main/java/org/moviematchers/moviematch/entity/Session.java
+++ server/src/main/java/org/moviematchers/moviematch/entity/Session.java
@@ -24,21 +24,15 @@ public class Session {
     @Column(name = "user_2_id")
     private Long user2ID;
 
-    @Column(name = "user_1_liked_movies")
-    private String User1LikedMovies; // ex. "3 5 6 8"
-    @Column(name = "user_2_liked_movies")
-    private String User2LikedMovies;
 
     public Session() {
     }
 
-    public Session(Long sessionID, Long invitationID, Long user1ID, Long user2ID, String user1LikedMovies, String user2LikedMovies) {
+    public Session(Long sessionID, Long invitationID, Long user1ID, Long user2ID) {
         this.sessionID = sessionID;
         this.invitationID = invitationID;
         this.user1ID = user1ID;
         this.user2ID = user2ID;
-        User1LikedMovies = user1LikedMovies;
-        User2LikedMovies = user2LikedMovies;
     }
 
     public Long getSessionID() {
@@ -73,19 +67,4 @@ public class Session {
         this.user2ID = user2ID;
     }
 
-    public String getUser1LikedMovies() {
-        return User1LikedMovies;
-    }
-
-    public void setUser1LikedMovies(String user1LikedMovies) {
-        User1LikedMovies = user1LikedMovies;
-    }
-
-    public String getUser2LikedMovies() {
-        return User2LikedMovies;
-    }
-
-    public void setUser2LikedMovies(String user2LikedMovies) {
-        User2LikedMovies = user2LikedMovies;
-    }
 }
