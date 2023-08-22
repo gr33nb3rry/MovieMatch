89777067f541de8edf2b381ee37749ee0d18d05e
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2023-08-22 13:58:55 +0300

    Chenged js methods to update random quote and friendlist

--- application/script/home.js
+++ application/script/home.js
@@ -1,20 +1,96 @@
-//const currentUserId;
-getCurrentUserID();
+const basicAuth = "Basic YWRtaW46YWRtaW4";
+let userToken;
+let userID = 6;
+let friendlist = [];
 
-function getCurrentUserID() {
-    const apiUrl = `http://localhost:8080/user/getLoginID`;
-    let headers = new Headers()
-    headers.append("Authorization", "Basic QmlnUHJpbmNlOnRlc3Q=")
-    fetch(apiUrl, { 
-        //mode: 'no-cors',
-        headers: headers }
-    )
-      .then(response => console.log(response))
-      .catch(error => {
-          console.error("ERROR:", error);
-      });
+getToken();
+getRandomQuote();
+getFriends();
+
+function getToken() {
+    fetch('http://localhost:8080/authorization/token', {
+        method: 'GET',
+        headers: {
+            'Authorization': basicAuth
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.token);
+        userToken = "Bearer " + data.token;
+    })
+    .catch(err => console.error(err));
 }
 
-function addFriend() {
-    const userIdInvited = document.getElementById('user_id').value;
+
+function getRandomQuote(){
+    const quote = document.getElementById('quote');
+
+    fetch('http://localhost:8080/quote/random', {
+        method: 'GET',
+        headers: {
+            'Authorization': basicAuth
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        quote.innerHTML = `${data.quoteText}<br>${data.movieTitle} (${data.movieYear})`;
+    })
+    .catch(err => console.error(err));
+}
+
+function getFriends() {
+
+    const friendshipsUrl = 'http://localhost:8080/friendship/byID?id=' + userID;
+    fetch(friendshipsUrl, {
+        method: 'GET',
+        headers: {
+            'Authorization': basicAuth
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        for(let i = 0; i < data.length; i++) {
+            let friendId;
+            let friendUsername;
+            
+            if (data[i].user1ID.userID === userID) {
+                friendId = data[i].user2ID.userID;
+                friendUsername = data[i].user2ID.userName;
+            }
+            else {
+                friendId = data[i].user1ID.userID;
+                friendUsername = data[i].user1ID.userName;
+            }
+            friendlist.push([friendId, friendUsername]);
+        }
+        updateFriendlist();
+    })
+    .catch(err => console.error(err));
+}
+
+function updateFriendlist() {
+    const friendlistContainer = document.getElementById("invite_friend_container");
+    friendlistContainer.innerHTML = `<h2 id="header_up_friend" > Find movie with friend </h2>`;
+
+    for(let i = 0; i < friendlist.length; i++) {
+        const friend = 
+        `
+        <div class="friend">
+            <div class="friend_left_side">
+                <div class="friend_picture" style="background-color: white;">
+
+                    <img src="asset/profile.png" class="header_button_icon" width="30px" height="30px" >
+                </div>
+                <p class="friend_name">${friendlist[i][1]}</p>
+            </div>
+            <a href="#popup-filters"><div class="friend_picture" style="background-color: #faae2b;">
+                <img src="asset/popcorn.png"  width="25px" height="30px">
+            </div></a>
+        </div>
+        `
+        friendlistContainer.innerHTML += friend;
+    }
 }
