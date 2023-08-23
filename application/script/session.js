a2b222fc5493747e2a1f124aa043ce81ca5b120e
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2023-08-24 00:14:45 +0300

    added method to add movies (should be initially) and method to get first movie

--- application/script/session.js
+++ application/script/session.js
@@ -4,3 +4,39 @@ let sessionID = parseInt(localStorage.getItem("sessionID"));
 let sessionUserID = parseInt(localStorage.getItem("sessionUserID"));
 console.log(sessionID);
 console.log(sessionUserID);
+
+addMovie();
+
+function addMovie() {
+    const url = 'http://localhost:8080/session/addMovies?sessionID='+sessionID;
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': basicAuth
+        }
+    })
+    .then(response => response.text())
+    .then((text) => {
+        console.log(text);
+        getCurrentMovie();
+    })
+    .catch(err => console.error(err));
+}
+function getCurrentMovie() {
+    const url = 'http://localhost:8080/session/getCurrent?sessionID='+sessionID+'&userNumber='+sessionUserID;
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': basicAuth
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.error(err));
+}
