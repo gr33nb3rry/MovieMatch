c94aa7af62a98c551f1b822c99629afe72fe13ed
lukofskis <lukas.borinskij@gmail.com>
2023-08-24 20:05:01 +0300

    Add the folder, assing gender icon by movie
    Add the gender icon folder, one is missing the Science_Fiction  icon! Also,try to write a methods, need to be upgrade, don't work

--- application/script/session.js
+++ application/script/session.js
@@ -136,3 +136,84 @@ function returnMovie() {
     })
     .catch(err => console.error(err));
 }
+
+
+///For genres icons change by movies
+
+async function fetchMovieData(movieId) {
+    const response = await fetch(`?????${movieId}`);
+    const data = await response.json();
+    return data;
+}
+
+
+async function assignImageByGenre() {
+    const code ='';
+    const movieData = await fetchMovieData(movieId);
+    const genre = movieData.genre;
+
+    switch (genre) {
+        case 'ACTION':
+             code = '<img src="asset/genres/action.png">';
+            break;
+        case 'ADVENTURE':
+             code = '<img src="asset/genres/adventure.png">';
+            break;
+        case 'ANIMATION':
+             code = '<img src="asset/genres/animation.png">';
+            break;
+        case 'COMEDY':
+             code = '<img src="asset/genres/comendy.png">';
+            break;
+        case 'CRIME':
+             code = '<img src="asset/genres/crime.png">';
+            break;
+        case 'DOCUMENTARY':
+             code = '<img src="asset/genres/documentory.png">';
+            break;
+        case 'DRAMA':
+             code = '<img src="asset/genres/drama.png">';
+            break;
+        case 'FAMILY':
+            const code = '<img src="asset/genres/family.png">';
+            break;
+        case 'FANTASY':
+             code = '<img src="asset/genres/fantasy.png">';
+            break;
+        case 'HISTORY':
+             code = '<img src="asset/genres/history.png">';
+            break;
+        case 'HORROR':
+             code = '<img src="asset/genres/horror.png">';
+            break;
+        case 'MUSIC':
+             code = '<img src="asset/genres/music.png">';
+            break;
+        case 'MYSTERY':
+             code = '<img src="asset/genres/mystery.png">';
+            break;
+        case 'ROMANCE':
+             code = '<img src="asset/genres/romance.png">';
+            break;
+        case 'SCIENCE_FICTION':
+             code = '<img src="asset/genres/BLOOOOO.png">';
+            //  one missing
+            break;
+        case 'TV':
+             code = '<img src="asset/genres/tv.png">';
+            break;
+        case 'THRILLER':
+             code = '<img src="asset/genres/thriller.png">';
+            break;
+        case 'WAR':
+             code = '<img src="asset/genres/war.png">';
+            break;
+        case 'WESTERN':
+             code = '<img src="asset/genres/western.png">';
+            break;
+        default:
+            log.console('Error');
+            break;
+    }
+   
+}
